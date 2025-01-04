e64639aa7ad8eee2be733f74c76e3df44d0c485f
Jay Tailor <jay8460568088@gmail.com>
2025-01-04 18:02:11 +0530

    Added Github Intergration

--- src/components/GitHubIcon.tsx
+++ src/components/GitHubIcon.tsx
@@ -3,7 +3,7 @@ import { Github } from "lucide-react";
 const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com"
+      href="https://github.com/Jaytailor15/global-time-zone"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed top-4 right-4 sm:top-6 sm:right-6 md:top-8 md:right-8 z-50 bg-card/80 p-2 rounded-full border border-gray-400 hover:bg-card-hover transition-colors duration-200"
